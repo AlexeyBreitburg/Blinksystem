7ef57c10d65a68f309b736cc0662dd333d3f5b9a
Ketsu8 <ketsoo@inbox.ru>
2017-10-08 02:11:23 +0300

    Removed all text :D

--- Utilities/blinkUpdate.py
+++
@@ -1,3 +0,0 @@
-import requests
-moduleName = requests.head("https://raw.githubusercontent.com/Blinkhub/blinkModules/master/" + args.install + ".zip")
-if moduleName.status_code == requests.codes.ok:
